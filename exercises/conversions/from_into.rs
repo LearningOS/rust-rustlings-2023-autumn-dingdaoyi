c8c185b2c2dda6f550e5ac0f68c969877697c226
岩冰 <1493001032@qq.com>
2023-10-09 17:39:02 +0800

    完工,ok 记录几个, 1, ffi有点不是太懂 rust to rust 2, build.rs 需要再重新学下 3, 范型trait 问题大点

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+use std::num::ParseIntError;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,12 +42,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if let Some((name, age_str)) = s.split_once(',') {
+            if name.is_empty() {
+                return Default::default();
+            }
+
+            if let Ok(age) = age_str.trim().parse() {
+                return Person {
+                    name: name.to_string(),
+                    age,
+                };
+            }
+        }
+
+        Default::default()
     }
 }
+
 
 fn main() {
     // Use the `from` function
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        if let Some((name, age_str)) = s.split_once(',') {
+
+            if name.is_empty() {
+                return Err(ParsePersonError::NoName);
+            }
+            if age_str.contains(',') {
+                return Err(ParsePersonError::BadLen);
+            }
+            return match age_str.trim().parse() {
+                Ok(age) => Ok(Person {
+                    name: name.to_string(),
+                    age,
+                }),
+                Err(msg) => Err(ParsePersonError::ParseInt(msg)),
+            };
+        }
+        return Err(ParsePersonError::BadLen);
     }
 }
 
@@ -68,6 +88,7 @@ mod tests {
     fn empty_input() {
         assert_eq!("".parse::<Person>(), Err(ParsePersonError::Empty));
     }
+
     #[test]
     fn good_input() {
         let p = "John,32".parse::<Person>();
@@ -76,6 +97,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 32);
     }
+
     #[test]
     fn missing_age() {
         assert!(matches!(
